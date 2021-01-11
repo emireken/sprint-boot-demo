8f89ee75aa2505ce31dfe2001ff1dd4ecfef02cf
Emir Eken <emirgokberk.eken@gmail.com>
2021-01-10 21:38:35 -0500

    update

--- target/classes/db/migration/V1__PersonTable.sql
+++ target/classes/db/migration/V1__PersonTable.sql
@@ -1,5 +1,19 @@
-CREATE TABLE person (
-    id UUID NOT NULL PRIMARY KEY,
-    name VARCHAR(100)  NOT NULL
+--CREATE TABLE person (
+--    id UUID NOT NULL PRIMARY KEY,
+--    name VARCHAR(100)  NOT NULL
+--);
+--
+----
+CREATE TABLE weather (
+weatherId INT,
+temperature INT,
+weatherMain VARCHAR,
+weatherIcon VARCHAR,
+weatherDescription VARCHAR,
+countryCode VARCHAR,
+name NAME,
+timestamp INT,
+sunrise INT,
+sunset INT
 );
 
